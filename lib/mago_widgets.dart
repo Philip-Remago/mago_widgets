99abd9d445174140cc130913021c0561f0b5a2b7
Philip Viljoen <philip.viljoen@remago.com>
2026-06-25 17:28:27 +0400

    chore: remove unused widgets and package imports

--- lib/mago_widgets.dart
+++ lib/mago_widgets.dart
@@ -5,8 +5,6 @@ export 'src/config/theme.dart';
 export 'src/helpers/border_radius_calculator.dart';
 export 'src/helpers/constants.dart';
 
-export 'src/services/pdf_service.dart';
-
 export 'src/widgets/buttons/button.dart';
 export 'src/widgets/buttons/button_style.dart';
 export 'src/widgets/buttons/icon_button.dart';
@@ -30,8 +28,6 @@ export 'src/widgets/components/qr.dart';
 export 'src/widgets/components/spinning_loader.dart';
 export 'src/widgets/components/toast.dart';
 
-export 'src/widgets/conference/conference.dart';
-
 export 'src/widgets/dialogs/action_popup_dialog.dart';
 export 'src/widgets/dialogs/large_action_popup_dialog.dart';
 export 'src/widgets/dialogs/options_popup_dialog.dart';
@@ -50,13 +46,6 @@ export 'src/widgets/inputs/image_file_input.dart';
 export 'src/widgets/inputs/password_input.dart';
 export 'src/widgets/inputs/search_dropdown.dart';
 export 'src/widgets/inputs/text_input.dart';
-
-export 'src/widgets/objects/images.dart';
-export 'src/widgets/objects/model.dart';
-export 'src/widgets/objects/note.dart';
-export 'src/widgets/objects/pdf.dart';
-export 'src/widgets/objects/web.dart';
-export 'src/widgets/objects/youtube_player.dart';
 
 export 'src/widgets/painters/grid_space_painter.dart';
 
